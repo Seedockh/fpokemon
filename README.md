6bc36eb4f09119ce6b8736b62f74b3e6cbcf1d2d
pierr <pierre.herisse@gmail.com>
2020-09-08 13:44:13 +0200

    both routes working, now going FP

---
+++ README.md
@@ -0,0 +1,12 @@
+# Functional Programming
+
+### Pokemons API
+
+**API used :** https://pokeapi.co/
+
+### Run project
+
+```console
+fpokemon$ yarn
+fpokemon$ ts-node ./src/main.ts
+```
--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,59 @@ import * as fs from 'fs'
 // Frontend template engine
 const vash = require('vash')
 
+// Views
+const index = fs.readFileSync('./views/index.vash').toString()
+const details = fs.readFileSync('./views/details.vash').toString()
+
+type ListPokemon = { name: string, url: string }
+
 const host = 'localhost'
 const port = 3000
 
-http.createServer((req,res) => {
+const getDetails = async (url: string) => {
+    return new Promise((resolve, reject) => {
+        const request = https.get(url, pokeResult => {
+            let pokeData = ''
+            pokeResult.on('data', chunk => pokeData += chunk)
+            pokeResult.on('end', () => resolve(JSON.parse(pokeData)))
+        })
+        request.end()
+    })
+}
+
+http.createServer(async (req,res) => {
     const url = req.url
     res.writeHead(200, { 'Content-Type': 'text/html' })
 
     if (url === '/') {
-        https.get("https://pokeapi.co/api/v2/pokemon", result => {
+        const allPokemons = https.get("https://pokeapi.co/api/v2/pokemon", result => {
             let data = ''
             result.on('data', chunk => data += chunk)
-            result.on('end', () => {
-                const apiData = JSON.parse(data)
-                const file = fs.readFileSync('./public/index.vash').toString()
-                const view = vash.compile(file)
+            result.on('end', async () => {
+                let apiData = JSON.parse(data)
+                for (let pokemon of apiData.results) {
+                    const details: any = await getDetails(pokemon.url)
+                    pokemon.image = details.sprites.front_default
+                    pokemon.id = details.id
+                }
 
-                res.end(view({ pokemons: apiData.results }))
+                const view = vash.compile(index)
+                view({ pokemons: apiData.results }, function layout(err, lyt) {
+                    res.write(lyt.finishLayout());
+                    res.end();
+                })
             })
+        })
+    }
+
+    if (url !== '/' && url !== '/favicon.ico') {
+        const pokemonId = url.match(/[0-9][0-9]?/gm)[0].toString()
+        const pokemonDetails: any = await getDetails(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+
+        const view = vash.compile(details)
+        view({ details: pokemonDetails }, function layout(err, lyt) {
+            res.write(lyt.finishLayout());
+            res.end();
         })
     }
 }).listen(port, host, () => {
